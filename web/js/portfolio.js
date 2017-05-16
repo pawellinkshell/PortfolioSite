4758f1a01760cea8b85e532b05679d28b9703758
Jan Koszela <jan.koszela@hicron.com>
2017-05-16 15:10:35 +0200

    Add essential components

---
+++ web/js/portfolio.js
@@ -0,0 +1,30 @@
+function scrollTo(event) {
+  if (event != 0) {
+  
+    var appElement = document.querySelector('[ng-app=portfolioApp]');
+    var elem = event.target.getAttribute('href');
+
+    var $anchor = $(appElement).find(elem);
+    $('html, body').stop().animate({
+      scrollTop: ($anchor.offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+  }
+}
+
+(function ($) {
+  "use strict"; // Start of use strict
+  // jQuery for page scrolling feature - requires jQuery Easing plugin
+
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+    target: '.navbar-fixed-top',
+    offset: 51
+  });
+
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function () {
+    $('.navbar-toggle:visible').click();
+  });
+
+})(jQuery); // End of use strict
